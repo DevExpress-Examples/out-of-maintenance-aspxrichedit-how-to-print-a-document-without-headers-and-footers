eaf1fab06422e62efbc3310e41a2768909037c71
CORP\alakina.svetlana <svetlana.rebrova@devexpress.com>
2018-12-26 13:06:44 +0300

    add example content

---
+++ CS/Default.aspx
@@ -0,0 +1,40 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<%@ Register assembly="DevExpress.Web.ASPxRichEdit.v18.2, Version=18.2.4.0, Culture=neutral, PublicKeyToken=b88d1754d700e49a" namespace="DevExpress.Web.ASPxRichEdit" tagprefix="dx" %>
+<%@ Register assembly="DevExpress.Web.v18.2, Version=18.2.4.0, Culture=neutral, PublicKeyToken=b88d1754d700e49a" namespace="DevExpress.Web" tagprefix="dx" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+     <script type="text/javascript">
+        function onCustomCommandExecuted(s, e) {
+            switch (e.commandName) {
+                case "Print":
+                    var url =  window.location.href + '?print=true';
+                    if(ASPx.PDFPluginHelper.IsInstalled()) {
+                        var helperFrame = document.createElement('iframe');
+                        helperFrame.name = 'printiframe';
+                        helperFrame.style.position = 'absolute'
+                        helperFrame.style.top = '-1000px'
+                        document.body.appendChild(helperFrame);
+                        window.frames[helperFrame.name].document.location = url;
+                    } else
+                        window.open(url);
+                    break;
+            }
+        }
+    </script>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <dx:ASPxRichEdit ID="ASPxRichEdit1" runat="server" WorkDirectory="~\App_Data\WorkDirectory">
+            <Settings>
+                <Behavior CreateNew="Disabled" Save="Disabled" SaveAs="Disabled" Open="Disabled" />
+            </Settings>
+            <ClientSideEvents CustomCommandExecuted="onCustomCommandExecuted" />
+        </dx:ASPxRichEdit>
+    </form>
+</body>
+</html>
---
+++ CS/Default.aspx.cs
@@ -0,0 +1,89 @@
+﻿using DevExpress.Web;
+using DevExpress.Web.ASPxThemes;
+using DevExpress.Web.Office;
+using DevExpress.XtraRichEdit;
+using DevExpress.XtraRichEdit.API.Native;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    private string docID = "document1";
+    protected void Page_Load(object sender, EventArgs e)
+    {      
+        if (!IsPostBack)
+        {
+            DocumentManager.CloseAllDocuments();
+            ASPxRichEdit1.Open(Server.MapPath("test.docx"));    
+            ASPxRichEdit1.DocumentId = docID;
+        }
+
+        ASPxRichEdit1.CreateDefaultRibbonTabs(true);
+
+        ASPxRichEdit1.RibbonTabs[0].Groups[0].Items.RemoveAt(4);
+        var newItem = new RibbonButtonItem("Print");
+        newItem.LargeImage.IconID = IconID.PrintPrintdialog32x32;
+        newItem.Size = RibbonItemSize.Large;
+        ASPxRichEdit1.RibbonTabs[0].Groups[0].Items.Add(newItem);
+
+        if (Request.Params["print"] == "true")
+        {
+            MemoryStream stream = new MemoryStream();
+            DocumentManager.FindDocument(docID).SaveCopy(stream);
+            RichEditDocumentServer server = new RichEditDocumentServer();
+            stream.Position = 0;
+            server.LoadDocument(stream, DocumentFormat.OpenXml);
+            foreach(Section section in server.Document.Sections)
+            {
+                UpdateFieldsInHeader(section, HeaderFooterType.Primary);
+                UpdateFieldsInHeader(section, HeaderFooterType.First);
+                UpdateFieldsInHeader(section, HeaderFooterType.Even);
+                UpdateFieldsInHeader(section, HeaderFooterType.Odd);
+                UpdateFieldsInFooter(section, HeaderFooterType.Primary);
+                UpdateFieldsInFooter(section, HeaderFooterType.First);
+                UpdateFieldsInFooter(section, HeaderFooterType.Even);
+                UpdateFieldsInFooter(section, HeaderFooterType.Odd);
+            }
+          
+            DevExpress.XtraPrinting.PdfExportOptions opt = new DevExpress.XtraPrinting.PdfExportOptions();
+            opt.ShowPrintDialogOnOpen = true;
+
+            MemoryStream ps = new MemoryStream();
+            server.ExportToPdf(ps, opt);
+            Response.Clear();
+            Response.ContentType = "application/pdf";
+            Response.AddHeader("Content-Disposition", "inline; filename=print.pdf");
+            Response.BinaryWrite(ps.ToArray());
+            Response.Flush();
+            Response.Close();
+            Response.End();
+            return;
+        }
+       }
+
+        private void UpdateFieldsInHeader(Section section, HeaderFooterType type)
+        {
+            if (section.HasHeader(type))
+            {
+                SubDocument header = section.BeginUpdateHeader(type);
+                header.Replace(header.Range, string.Empty);
+                section.EndUpdateHeader(header);
+            }
+        }
+
+        private void UpdateFieldsInFooter(Section section, HeaderFooterType type)
+        {
+            if (section.HasFooter(type))
+            {
+                SubDocument footer = section.BeginUpdateFooter(type);
+                footer.Replace(footer.Range, string.Empty);
+                section.EndUpdateFooter(footer);
+            }
+        }   
+
+}
